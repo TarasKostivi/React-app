d20ef98763b8a2954580ab72f9a503a660e82c9d
TarasKostivi <taraskostsv@gmail.com>
2022-02-22 16:17:08 +0200

    12 less

---
+++ src/Techologies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Techologies = () => {
+  return (
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>
+  );
+}
+export default Techologies;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src='https://get-edu.kz/wp-content/uploads/2020/04/helpbox-contact.jpg' alt='img' />
+    </div>
+    <div>
+      ava + decs
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>;
+}
+
+export default Profile;
